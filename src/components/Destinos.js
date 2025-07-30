a6e6da37d9581a992d6b76d359384f43ad004579
Francisco Burtin <francisco.burtin@cscgfm.com>
2025-07-30 09:20:39 -0300

    Add new

--- src/components/Destinos.js
+++ src/components/Destinos.js
@@ -11,7 +11,18 @@ function Destinos() {
     <section id="destinos" className="py-5 bg-light">
       <div className="container">
         <h2 className="text-center mb-4">Destinos Populares</h2>
-      
+        <div className="row">
+          {destinos.map((destino, index) => (
+            <div key={index} className="col-md-4 mb-4">
+              <div className="card h-100 shadow-sm">
+                <img src={destino.imagen} alt={destino.nombre} className="card-img-top" />
+                <div className="card-body">
+                  <h5 className="card-title text-center">{destino.nombre}</h5>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
