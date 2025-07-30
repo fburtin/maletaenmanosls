5ed509848f8a7f27c97bc2854ddc0a37366eed3e
Francisco Burtin <francisco.burtin@cscgfm.com>
2025-07-30 14:36:37 -0300

    Changed all image because of size and add more.

--- src/components/Destinos.js
+++ src/components/Destinos.js
@@ -6,12 +6,16 @@ function Destinos() {
       country: "Argentina",
       videos: [
         {
-          src: "/videos/Argentina/4984759-hd_1920_1080_30fps.mp4",
+          src: "/videos/Argentina/4984759-sd_640_360_30fps.mp4",
           title: "Misiones Cataratas del Iguazú"
         },
         {
-          src: "/videos/Argentina/5408391-hd_1920_1080_30fps.mp4",
+          src: "/videos/Argentina/5408391-sd_640_360_30fps.mp4",
           title: "Pasarela Cataratas"
+        },
+        {
+          src: "/videos/Argentina/13735575_640_360_24fps.mp4",
+          title: "Misiones Cataratas del Iguazú"
         }
       ]
     },
@@ -19,12 +23,16 @@ function Destinos() {
       country: "Brasil",
       videos: [
         {
-          src: "/videos/Brazil/13326487-hd_1920_1080_30fps.mp4",
+          src: "/videos/Brazil/13326487-sd_640_360_30fps.mp4",
           title: "Rio de Janeiro"
         },
         {
-          src: "/videos/Brazil/4902988-hd_1920_1080_25fps.mp4",
+          src: "/videos/Brazil/3127125-sd_640_360_30fps.mp4",
           title: "Cristo"
+        },
+        {
+          src: "/videos/Brazil/13326453-sd_640_360_30fps.mp4",
+          title: "Playas"
         }
       ]
     },
@@ -32,12 +40,16 @@ function Destinos() {
       country: "Francia",
       videos: [
         {
-          src: "/videos/France/2034261-hd_1920_1080_30fps.mp4",
+          src: "/videos/France/2034261-sd_640_360_30fps.mp4",
           title: "Torre Eiffel Paris"
         },
         {
-          src: "/videos/France/3703770-hd_1920_1080_30fps.mp4",
+          src: "/videos/France/3703770-sd_640_360_30fps.mp4",
           title: "Museo Louvre Paris"
+        },
+        {
+          src: "/videos/France/8531830-sd_640_360_25fps.mp4",
+          title: "Quesos"
         }
       ]
     },
@@ -45,12 +57,16 @@ function Destinos() {
       country: "Italia",
       videos: [
         {
-          src: "/videos/Italy/20606535-hd_1920_1080_24fps.mp4",
+          src: "/videos/Italy/13447472_640_360_25fps.mp4",
           title: "Roma Coliseo"
         },
         {
-          src: "/videos/Italy/4840581-hd_1920_1080_25fps.mp4",
-          title: "Roma La Fuente de Trevi"
+          src: "/videos/Italy/5912737-sd_640_360_30fps.mp4",
+          title: "terraza del Gianicolo (en italiano, Janiculum)"
+        },
+        {
+          src: "/videos/Italy/5979469-sd_640_360_30fps.mp4",
+          title: "Venecia"
         }
       ]
     },
@@ -58,15 +74,15 @@ function Destinos() {
       country: "Marruecos",
       videos: [
         {
-          src: "/videos/Morocco/3015519-hd_1920_1080_24fps.mp4",
+          src: "/videos/Morocco/3015519-sd_640_360_24fps.mp4",
           title: "Mezquita Kutubía"
         },
         {
-          src: "/videos/Morocco/3115231-hd_1920_1080_24fps.mp4",
+          src: "/videos/Morocco/3115231-sd_640_360_24fps.mp4",
           title: "Marruecos Negocio"
         },
         {
-          src: "/videos/Morocco/5442766-hd_1920_1080_25fps.mp4",
+          src: "/videos/Morocco/5442766-sd_640_360_25fps.mp4",
           title: "Marruecos Desierto del Sahara"
         }
       ]
@@ -75,12 +91,16 @@ function Destinos() {
       country: "Portugal",
       videos: [
         {
-          src: "/videos/Portugal/19052363-hd_1920_1080_25fps.mp4",
+          src: "/videos/Portugal/19052363-sd_640_360_25fps.mp4",
           title: "Santuario Nacional de Cristo Rey"
         },
         {
-          src: "/videos/Portugal/4820471-hd_1920_1080_25fps.mp4",
+          src: "/videos/Portugal/19909764-sd_640_360_30fps.mp4",
           title: "Marques Do Pombal"
+        },
+        {
+          src: "/videos/Portugal/5738602-sd_640_360_24fps.mp4",
+          title: "Playa de Benagil"
         }
       ]
     },
@@ -88,12 +108,16 @@ function Destinos() {
       country: "España",
       videos: [
         {
-          src: "/videos/Spain/17579220-hd_1920_1080_25fps.mp4",
+          src: "/videos/Spain/17579220-sd_640_360_25fps.mp4",
           title: "Andalucía - Puente Nuevo. Centro de Interpretación"
         },
         {
-          src: "/videos/Spain/7069320-hd_2048_1080_24fps.mp4",
-          title: "Madrid"
+          src: "/videos/Spain/8441280-sd_640_360_25fps.mp4",
+          title: " Plaza de Colón en Madrid"
+        },
+        {
+          src: "/videos/Spain/4243914-sd_640_360_30fps.mp4",
+          title: "Palacio Real de Madrid"
         }
       ]
     },
@@ -101,12 +125,16 @@ function Destinos() {
       country: "Suiza",
       videos: [
         {
-          src: "/videos/Switzerland/3512545-hd_1920_1080_25fps.mp4",
+          src: "/videos/Switzerland/3512545-sd_640_360_25fps.mp4",
           title: "Alpes Suizos"
         },
         {
-          src: "/videos/Switzerland/3853624-hd_1920_1080_30fps.mp4",
+          src: "/videos/Switzerland/3655304-sd_640_360_30fps.mp4",
           title: "Berna"
+        },
+        {
+          src: "/videos/Switzerland/3217373-sd_640_360_25fps.mp4",
+          title: "Alpes Suizos"
         }
       ]
     }
@@ -114,31 +142,33 @@ function Destinos() {
 
     return (
     <div className="container mt-5 text-center">
-      {gallery.map((item, idx) => (
+        {gallery.map((item, idx) => (
         <div key={idx} className="mb-5">
-          <h3  className="country-title mb-4">{item.country}</h3>
-          <div className="row justify-content-center">
+            <h3 className="country-title mb-4">{item.country}</h3>
+            <div className="row justify-content-center">
             {item.videos.map((video, index) => (
-              <div key={index} className="col-md-4 mb-4 d-flex flex-column align-items-center">
+                <div key={index} className="col-md-4 mb-4 d-flex flex-column align-items-center">
                 <video
-                  controls
-                  autoPlay
-                  muted
-                  loop
-                  preload="metadata"
-                  style={{ width: "100%", borderRadius: "8px" }}
+                    autoPlay
+                    muted
+                    loop
+                    preload="metadata"
+                    playsInline // evita pantalla completa automática en móviles
+                    disablePictureInPicture // evita PIP en navegadores compatibles
+                    controlsList="nodownload nofullscreen noremoteplayback"
+                    style={{ width: "100%", borderRadius: "8px", pointerEvents: 'none' }} // pointerEvents evita clics en el video
                 >
-                  <source src={video.src} type="video/mp4" />
-                  Tu navegador no soporta el video.
+                    <source src={video.src} type="video/mp4" />
+                    Tu navegador no soporta el video.
                 </video>
                 <small className="mt-2 text-muted">{video.title}</small>
-              </div>
+                </div>
             ))}
-          </div>
+            </div>
         </div>
-      ))}
+        ))}
     </div>
-  );
+    );
 }
 
 export default Destinos;
